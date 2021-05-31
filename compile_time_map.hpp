88e063c6b1ec1d924130b04120670612315e6381
timog <timo.glane@gmail.com>
2021-05-31 20:16:19 +0200

    ref/fix:Compile time map improvements

--- compile_time_map.hpp
+++ compile_time_map.hpp
@@ -2,12 +2,11 @@
 #define CPP_UTILITY_COMPILE_TIME_MAP_HPP
 
 #include <array>
-#include <string_view>
 
 namespace utility
 {
 
-template <typename KEY, typename VALUE, size_t SIZE>
+template <typename KEY, typename VALUE, std::size_t SIZE>
 class compile_time_map
 {
     using key_type = KEY;
@@ -15,27 +14,28 @@ class compile_time_map
 
 public:
 
-    constexpr compile_time_map() = default;
-
-    constexpr compile_time_map(const std::array<std::pair<key_type, value_type>, SIZE>& data)
-        : buffer_ {data}
+    template <typename ... PAIR>
+    constexpr compile_time_map(PAIR&& ... pairs)
+        : m_buffer {static_cast<PAIR &&>(pairs) ...}
     {}
 
-    constexpr bool insert(const key_type& key, const value_type& value)
-    {
-        // TODO
-        return false;
-    }
+    constexpr compile_time_map(const std::array<std::pair<key_type, value_type>, SIZE> &data)
+        : m_buffer {data}
+    {}
 
-    constexpr bool insert_or_assign(const key_type& key, const value_type& value)
+    constexpr value_type operator[](const key_type& key) const
     {
-        // TODO
-        return false;
+        for(const auto& pair : m_buffer)
+        {
+            if(pair.first == key)
+                return pair.second;
+        }
+        return {};
     }
 
     constexpr value_type at(const key_type& key) const
     {
-        for(const auto& pair : buffer_)
+        for(const auto& pair : m_buffer)
         {
             if(pair.first == key)
                 return pair.second;
@@ -45,7 +45,7 @@ public:
 
 private:
 
-    std::array<std::pair<key_type, value_type>, SIZE> buffer_;
+    std::array<std::pair<key_type, value_type>, SIZE> m_buffer;
 
 };
 
