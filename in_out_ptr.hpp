4107b345795d13d00f1fd3b65a95307cb4cbc64f
timog <timo.glane@gmail.com>
2021-07-09 14:31:51 +0200

    Add very simple not-finished version of out_ptr and in_out_ptr

---
+++ in_out_ptr.hpp
@@ -0,0 +1,49 @@
+#ifndef CPP_UTILITY_IN_OUT_PTR_HPP
+#define CPP_UTILITY_IN_OUT_PTR_HPP
+
+namespace utility {
+
+// TODO Currently only working with unique_ptr, so make it work with shared_ptr
+template<typename SMART, typename POINTER>
+class in_out_ptr
+{
+    using smart_ptr_type = SMART;
+    using ptr_type = POINTER;
+
+public:
+
+    in_out_ptr(smart_ptr_type& smart_ptr)
+        : m_smart {&smart_ptr},
+          m_ptr {smart_ptr.release()}
+    {}
+
+    ~in_out_ptr() noexcept
+    {
+        m_smart->reset(m_ptr);
+    }
+
+    operator ptr_type*() noexcept
+    {
+        return &m_ptr;
+    }
+
+    operator void**() noexcept
+    {
+        return reinterpret_cast<void**>(&m_ptr);
+    }
+
+private:
+
+    smart_ptr_type* m_smart;
+    ptr_type m_ptr;
+
+};
+
+/// Deduction guide for in_out_ptr type from a smart pointer
+template<typename SMART>
+in_out_ptr(SMART& smart) ->
+    in_out_ptr<typename std::remove_reference<decltype(smart)>::type, decltype(smart.get())>;
+
+} // namespace utility
+
+#endif
---
+++ out_ptr.hpp
@@ -0,0 +1,49 @@
+#ifndef CPP_UTILITY_OUT_PTR_HPP
+#define CPP_UTILITY_OUT_PTR_HPP
+
+namespace utility {
+
+// TODO Currently only working with unique_ptr, so make it work with shared_ptr
+template<typename SMART, typename POINTER>
+class out_ptr
+{
+    using smart_ptr_type = SMART;
+    using ptr_type = POINTER;
+
+public:
+
+    out_ptr(smart_ptr_type& smart)
+        : m_smart {&smart},
+          m_ptr {smart.get()}
+    {}
+
+    ~out_ptr() noexcept
+    {
+        m_smart->reset(m_ptr);
+    }
+
+    operator ptr_type*() noexcept
+    {
+        return &m_ptr;
+    }
+
+    operator void**() noexcept
+    {
+        return reinterpret_cast<void**>(&m_ptr);
+    }
+
+private:
+
+    smart_ptr_type* m_smart;
+    ptr_type m_ptr;
+
+};
+
+/// Deduction guide for out_ptr type from a smart pointer
+template<SMART>
+out_ptr(SMART& smart) ->
+    out_ptr<typename std::remove_reference<decltype(smart)>::type, decltype(smart.get())>;
+
+} // namespace utility
+
+#endif
